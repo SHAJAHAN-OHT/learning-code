5e16cdc87dddb9dbc8364177ac2492ad3683f00e
Shajahan <87851422+Shajahanhng@users.noreply.github.com>
2022-03-03 17:58:43 +0530

    post call

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,14 @@
 var express = require('express');  
+const fs = require('fs')
 var app = express();  
-app.post('/', function (req, res) {  
-   res.send('Welcome to JavaTpoint');  
-})  
+app.post('/', function(req,res){
+    console.log('file',req.file)
+    // fs.readFile(req.files.file.path, function(err, data){
+      // Do something with the data (which holds the file information)
+    //   console.log("data",data)
+    //   res.send(data)
+    // });
+  })  
 var server = app.listen(8000, function () {  
 var host = server.address().address  
   var port = server.address().port  
